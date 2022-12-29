724e8ef9698637dc83a29ac849401efe19b516f4
Gantavya Saraswat <gantavya99@gmail.com>
2022-12-30 00:41:24 +0530

    pushing same commit

--- src/views/Calendar/js/Calendar.js
+++ src/views/Calendar/js/Calendar.js
@@ -3,11 +3,9 @@ import { InlineWidget } from "react-calendly";
 const Calendar = () => {
   return (
     <div>
-        
-            <InlineWidget url="https://calendly.com/gantavya99/30min" />
-        
-
+        <InlineWidget url="https://calendly.com/gantavya99/30min" />
     </div>
+    
   )
 }
 
