82a0ffd1114acc764a8281287cf3c774e0d08b45
Gantavya Saraswat <gantavya99@gmail.com>
2023-01-19 10:10:48 +0530

    Fixed Profile page

--- src/views/Profile/js/Profile.js
+++ src/views/Profile/js/Profile.js
@@ -17,16 +17,20 @@ const Profile = () => {
   return (
     
     <div className="container d-flex align-items-center justify-content-center m-5 ">
-        <div className="user-box d-flex  justify-content-center">
+        <div className="user-box d-flex  align-items-center flex-column">
             <div>
             <h1>
-                My Profile
+            My Profile
             </h1>
+                
+            
             </div>
             <div>
             <h2>
-                UserName 
+            UserName 
             </h2>
+                
+            
             </div>
             <div>
             <button className="btn-primary" onClick={()=>signOutUser()}>
