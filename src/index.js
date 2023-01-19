de61ba51f61f0f247d712de84a0d18e2a90b5a2c
bhushanhr26 <58306435+bhushanhr26@users.noreply.github.com>
2023-01-19 20:41:19 +0530

    sentry.io integrated

--- src/index.js
+++ src/index.js
@@ -6,18 +6,23 @@ import reportWebVitals from "./reportWebVitals";
 import { FirebaseProvider } from "../src/context/Firebase";
 import { store } from "../src/Store/store.js";
 import { Provider } from "react-redux";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://2338fbbf5a0643c185f024bac3d680c3@o4504531724075008.ingest.sentry.io/4504531725516800", //paste copied DSN value here
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0, //lower the value in production
+});
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
- 
-    <Provider store={store}>
-      {/* <FirebaseProvider> */}
-      <App />
-      {/* </FirebaseProvider> */}
-      
-    </Provider>
- 
+  <Provider store={store}>
+    {/* <FirebaseProvider> */}
+    <App />
+    {/* </FirebaseProvider> */}
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
