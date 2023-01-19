ee4195db6aaef6849cfcee2bdee1ba2a6ce93559
Gantavya Saraswat <gantavya99@gmail.com>
2023-01-19 15:56:52 +0530

    Fixed Design of Sign Up and Login Screen

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,85 +1,60 @@
 import React from 'react';
-import GoogleIcon from '@mui/icons-material/Google';
+import logo from "./assets/NewIcon.png";
 import {
   MDBBtn,
   MDBContainer,
-  MDBRow,
-  MDBCol,
   MDBCard,
   MDBCardBody,
-  MDBInput,
-  MDBCheckbox,
-  MDBIcon
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBInput
 }
 from 'mdb-react-ui-kit';
-import "./components/Register/css/Register.css";
 
+import "./components/Register/css/Register.css"
 function App() {
   return (
-    <MDBContainer fluid className='p-4'>
+    <MDBContainer className="my-5 w-50">
 
-      <MDBRow>
+      <MDBCard className="mt-5" >
+        <MDBRow className='g-0'>
 
-        <MDBCol md='6' className='text-center text-md-start d-flex flex-column justify-content-center'>
+          <MDBCol md='6'>
+            <MDBCardImage src='https://images.unsplash.com/photo-1600880292089-90a7e086ee0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTJ8fGhlYWx0aGNhcmV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60' alt="login form" className='rounded-start w-100'/>
+          </MDBCol>
 
-          <h1 className="my-5 display-3 fw-bold ls-tight px-3">
-            The best Healthcare <br />
-            <span style={{color:"#025f4c"}}>platform around you</span>
-          </h1>
+          <MDBCol md='6'>
+            <MDBCardBody className='d-flex flex-column'>
 
-          <p className='px-3' style={{color: 'hsl(217, 10%, 50.8%)'}}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Eveniet, itaque accusantium odio, soluta, corrupti aliquam
-            quibusdam tempora at cupiditate quis eum maiores libero
-            veritatis? Dicta facilis sint aliquid ipsum atque?
-          </p>
-
-        </MDBCol>
-
-        <MDBCol md='6'>
-
-          <MDBCard className='my-5 w-50'>
-            <MDBCardBody className='p-5'>
-
-              {/* <MDBRow>
-                <MDBCol col='6'>
-                  <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text'/>
-                </MDBCol>
-
-                <MDBCol col='6'>
-                  <MDBInput wrapperClass='mb-4' label='Last name' id='form1' type='text'/>
-                </MDBCol>
-              </MDBRow> */}
-
-              <MDBInput wrapperClass='mb-4' label='Email' id='form1' type='email'/>
-              <MDBInput wrapperClass='mb-4' label='Password' id='form1' type='password'/>
-
-              <div className='d-flex justify-content-center mb-4'>
-                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='I agree to the terms of service and Privacy Policy' />
+              <div className='d-flex flex-row mt-2'>
+                <MDBIcon fas icon="cubes fa-3x" style={{ color: '#ff6219' }}/>
+                <span className="h1 fw-bold mb-0">Daac Saab</span>
               </div>
 
-              <MDBBtn className='w-100 mb-4' size='md'>Sign up</MDBBtn>
+              <h5 className="fw-normal my-4 pb-3" style={{letterSpacing: '1px'}}>Sign into your account</h5>
 
-              <div className="text-center">
+                <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
+                <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
 
-                <p>or sign up with:</p>
+                <MDBBtn className="w-100 mb-4" size="md">
+                Login
+              </MDBBtn>
+              <a className="small text-muted" href="#!">Forgot password?</a>
+              <br />
+              <p className="mb-5 pb-lg-2" style={{color: '#393f81'}}>Don't have an account? <a href="#!" style={{color: '#1d4ed8'}}>Register here</a></p>
 
-                
-
-                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#123fre' }}>
-                  <GoogleIcon color='red'/> Sign Up With Google
-                </MDBBtn>
-
-              
-
+              <div className='d-flex flex-row justify-content-start'>
+                <a href="#!" className="small text-muted me-1">Terms of use.</a>
+                <a href="#!" className="small text-muted">Privacy policy</a>
               </div>
 
             </MDBCardBody>
-          </MDBCard>
+          </MDBCol>
 
-        </MDBCol>
-
-      </MDBRow>
+        </MDBRow>
+      </MDBCard>
 
     </MDBContainer>
   );
--- src/components/Register/js/RegisterPage.jsx
+++ src/components/Register/js/RegisterPage.jsx
@@ -6,24 +6,34 @@ import {
   onAuthStateChanged,
   signInWithPopup,
 } from "firebase/auth";
-import { app} from "../../../context/Firebase";
+import GoogleIcon from "@mui/icons-material/Google";
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBCheckbox,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+import { app } from "../../../context/Firebase";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../css/Register.css";
 
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
-
-  const auth = getAuth(app);
-  const googleProvider =  new GoogleAuthProvider();
-
-
-
-  const RegisterPage = () => {
-
-  const navigate=useNavigate();
+//Functional Component begins here
+const RegisterPage = () => {
+  //hooks
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  //signUp Functions
   const signUpUser = () => {
     createUserWithEmailAndPassword(auth, email, password).then((value) =>
       navigate("/")
@@ -31,38 +41,87 @@ import "../css/Register.css";
   };
 
   const signInWithGoogle = () => {
-    signInWithPopup(auth,googleProvider).then(()=>{
-      navigate("/")
-    })
+    signInWithPopup(auth, googleProvider).then(() => {
+      navigate("/");
+    });
   };
-  
+
   return (
-    <div className="container register-container mt-5">
-      <div className="d-flex align-items-center justify-content-center flex-column ">
-        <label className="mt-5">Email</label>
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          type="email"
-          required
-          placeholder="Enter your email here"
-        />
-        <label>Password</label>
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-          type="password"
-          required
-          placeholder="Enter your Password"
-        />
-        <br />
-        <button className="btn-danger" onClick={signInWithGoogle}>Sign in with google</button>
-        <button className="btn-primary" onClick={signUpUser}>
-          Sign Up
-        </button>
-      </div>
-    </div>
+    <MDBContainer fluid className="p-4">
+      <MDBRow>
+        <MDBCol
+          md="6"
+          className="text-center text-md-start d-flex flex-column justify-content-center"
+        >
+          <h1 className="my-5 display-3 fw-bold ls-tight px-3">
+            The best Healthcare <br />
+            <span style={{ color: "#025f4c" }}>platform around you</span>
+          </h1>
+
+          <p className="px-3" style={{ color: "hsl(217, 10%, 50.8%)" }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+            itaque accusantium odio, soluta, corrupti aliquam quibusdam tempora
+            at cupiditate quis eum maiores libero veritatis? Dicta facilis sint
+            aliquid ipsum atque?
+          </p>
+        </MDBCol>
+
+        <MDBCol md="6">
+          <MDBCard className="my-5 w-50">
+            <MDBCardBody className="p-5">
+              {/* <MDBRow>
+              <MDBCol col='6'>
+                <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text'/>
+              </MDBCol>
+
+              <MDBCol col='6'>
+                <MDBInput wrapperClass='mb-4' label='Last name' id='form1' type='text'/>
+              </MDBCol>
+            </MDBRow> */}
+
+              <MDBInput
+                wrapperClass="mb-4"
+                label="Email"
+                id="form1"
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+              <MDBInput
+                wrapperClass="mb-4"
+                label="Password"
+                id="form1"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                required
+              />
+
+              <div className="d-flex justify-content-center mb-4">
+                <MDBCheckbox
+                  name="flexCheck"
+                  value=""
+                  id="flexCheckDefault"
+                  label="I agree to the terms of service and Privacy Policy"
+                />
+              </div>
+
+              <MDBBtn onClick={signUpUser} className="w-100 mb-4" size="md">
+                Sign up
+              </MDBBtn>
+
+              <div className="text-center">
+                <p>or sign up with:</p>
+
+                <button className="Google-btn p-2" onClick={signInWithGoogle}>
+                  <GoogleIcon /> Sign up with Google
+                </button>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 };
-
 export default RegisterPage;
